696b3ac58f652394b3f36a92432a1b4847522e85
Alano Terblanche <alanopi.314@gmail.com>
2019-08-10 21:48:47 +0200

    Camera class updated

--- Camera.py
+++ Camera.py
@@ -4,8 +4,8 @@ from APIHandler import APIHandler
 class Camera(APIHandler):
 
     def __init__(self, ip, username="admin", password=""):
-        APIHandler.__init__(self, ip)
+        APIHandler.__init__(self, ip, username, password)
         self.ip = ip
         self.username = username
         self.password = password
-        super().login(self.username, self.password)
+        super().login()
