e3c913cf26db9683efee52fc645aae2f9c68c685
Paul H. Breslin <phbcanada@users.noreply.github.com>
2023-10-14 11:41:03 -0400

    Correct call is actually GetMdAlarm
    GetAlarm is not supported on newer cameras.

--- reolinkapi/mixins/alarm.py
+++ reolinkapi/mixins/alarm.py
@@ -4,11 +4,22 @@ from typing import Dict
 class AlarmAPIMixin:
     """API calls for getting device alarm information."""
 
+    def get_alarm(self) -> Dict:
+        """
+        Gets the device alarm motion
+        See examples/response/GetAlarmMotion.json for example response data.
+        :return: response json
+        """
+        cmd = "GetAlarm"
+        body = [{"cmd": cmd, "action": 1, "param": {"Alarm": {"channel": 0, "type": "md"}}}]
+        return self._execute_command(cmd, body)
+
     def get_alarm_motion(self) -> Dict:
         """
         Gets the device alarm motion
         See examples/response/GetAlarmMotion.json for example response data.
         :return: response json
         """
-        body = [{"cmd": "GetAlarm", "action": 1, "param": {"Alarm": {"channel": 0, "type": "md"}}}]
-        return self._execute_command('GetAlarm', body)
+        cmd = "GetMdAlarm"
+        body = [{"cmd": cmd, "action": 1, "param": {"channel": 0}}]
+        return self._execute_command(cmd, body)
