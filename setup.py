fa89af1b9858607241fd4df84d2bf29e0e39290a
Fabio Testa <fabio.testa@cchellenic.com>
2024-08-09 17:56:55 +0200

    update setup version print import error

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name=NAME,
-    python_requires='>=3.6.0',
+    python_requires='>=3.12.4',
     version=find_version('reolinkapi', '__init__.py'),
     description=DESCRIPTION,
     long_description=long_description,
